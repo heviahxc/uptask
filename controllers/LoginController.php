b45c27425d54c600a4c510012e60f2f3c741dbb4
heviahxc <heviamc@gmail.com>
2023-04-16 17:19:09 -0400

    first commit

---
+++ controllers/LoginController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace Controllers;
+
+use MVC\Router;
+
+class LoginController{
+
+    public static function login(Router $router){
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            # code...
+        }
+    }
+
+    public static function logout(){
+
+    
+    }
+
+    public static function crear(){
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            # code...
+        }
+    }
+
+    public static function olvide(){
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            # code...
+        }
+    }
+
+    public static function reestablecer(){
+
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            # code...
+        }
+    }
+
+    public static function mensaje(){
+
+    
+    }
+
+    public static function confirmar(){
+
+    
+    }
+}
